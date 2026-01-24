cfbc8ea15b377bd472dc4b3e8cab65895b3b8011
Waleed <waleedeveloper100@gmail.com>
2026-01-24 16:16:50 +0500

    notes created

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import "./app.scss"
 import Dock from './components/dock/Dock'
 import Nav from './components/nav/Nav'
 import Github from './components/windows/Github'
+import Note from './components/windows/Note'
+import Resume from './components/windows/Resume'
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
-      <Github />
+      {/* <Github /> */}
+      {/* <Note /> */}
+      <Resume />
     </main>
   )
 }
